e87ec5a4e8061bf98e6dd0fb6e0a9a20dff08be5
Adrian-Marin <a18310176@live.ceti.mx>
2022-07-28 18:16:24 -0500

    VideoGame - Base
    Versión 1.0 del juego, no hay errores o defectos detectados hasta el momento.

--- Utopic Art Studios/Assets/Prefab/Menu/Script/InGameMenu.cs
+++ Utopic Art Studios/Assets/Prefab/Menu/Script/InGameMenu.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class InGameMenu : MonoBehaviour
 {
-    public GameObject canvasPausa;
     public GameObject panelPausa;
     public GameObject playerPrefab;
     public static bool isPaused;
@@ -26,23 +25,15 @@ public class InGameMenu : MonoBehaviour
     }
 
     public void PauseGame(){
-        if (canvasPausa){
-            canvasPausa.SetActive(true);
-            playerPrefab.GetComponent<PlayerController>().enabled = false;
-        }
+        playerPrefab.GetComponent<PlayerController>().enabled = false;
         panelPausa.SetActive(true);
-        canvasPausa.GetComponent<CanvasGroup>().alpha = 1f;
         isPaused = true;
         Time.timeScale = 0f;
     }
 
     public void ResumeGame(){
-        if (canvasPausa){
-            canvasPausa.SetActive(false);
-            playerPrefab.GetComponent<PlayerController>().enabled = true;
-        }
+        playerPrefab.GetComponent<PlayerController>().enabled = true;
         panelPausa.SetActive(false);
-        canvasPausa.GetComponent<CanvasGroup>().alpha = 0f;
         isPaused = false;
         Time.timeScale = 1f;
     }
